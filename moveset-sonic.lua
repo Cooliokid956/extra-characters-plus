cc5523b784f659afeb9920e88e7fa4dce3a401df
stvene3004 <steven30042007@gmail.com>
2026-01-10 16:04:33 +0700

    A buncha tiny Sonic fixes.
    - Made drowning sounds and visuals client-side only.
    - Added 2 failsafes to the homing attack. One kicks Sonic out of the homing state after 5 seconds, the other makes sure breakable objects actually break in contact the homing attack.

--- moveset-sonic.lua
+++ moveset-sonic.lua
@@ -788,7 +788,7 @@ local function act_homing_attack(m)
     local o = sonic_find_homing_target(m, 700)
     local yaw, pitch
 
-    if o == nil then
+    if o == nil or m.actionTimer > 150  then
         set_mario_action(m, ACT_SONIC_FALL, 3)
         e.sonic.actionADone = true
         return 1
@@ -841,6 +841,17 @@ local function act_homing_attack(m)
                     set_mario_action(m, ACT_SONIC_RUNNING, 0)
                 end
             end
+        end
+
+    elseif stepResult == AIR_STEP_HIT_WALL then
+        -- A failsafe in case the homing attack doesn't break boxes for some godforsaken reason.
+        if m.wall.object == o and o.oInteractType == INTERACT_BREAKABLE then
+            m.vel.x = sins(m.faceAngle.y) * -16
+            m.vel.z = coss(m.faceAngle.y) * -16
+            o.oInteractStatus = ATTACK_GROUND_POUND_OR_TWIRL + (INT_STATUS_INTERACTED | INT_STATUS_WAS_ATTACKED)
+            set_mario_action(m, ACT_AIR_SPIN, 0)
+            if m.playerIndex == 0 then set_camera_shake_from_hit(SHAKE_ATTACK) end
+            set_mario_particle_flags(m, PARTICLE_TRIANGLE, 0)
         end
     end
 
@@ -1319,11 +1330,13 @@ function sonic_drowning(m, e)
         sonic_set_alive(m)
         e.sonic.oxygen = e.sonic.oxygen - 1
 
-        if warning[e.sonic.oxygen] then
-            play_sound(SOUND_MOVING_ALMOST_DROWNING, m.marioObj.header.gfx.cameraToObject)
-        elseif getOutNow[e.sonic.oxygen] then
-            audio_sample_play(SOUND_COUNTDOWN_CD, m.pos, 2)
-            spawn_orange_number(getOutNow[e.sonic.oxygen], 0, 100, 0)
+        if m.playerIndex == 0 then
+            if warning[e.sonic.oxygen] then
+                play_sound(SOUND_MOVING_ALMOST_DROWNING, m.marioObj.header.gfx.cameraToObject)
+            elseif getOutNow[e.sonic.oxygen] then
+                audio_sample_play(SOUND_COUNTDOWN_CD, m.pos, 2)
+                spawn_orange_number(getOutNow[e.sonic.oxygen], 0, 100, 0)
+            end
         end
         --djui_chat_message_create(tostring(e.sonic.oxygen))
     else
